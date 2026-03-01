e52e80ff96284c1eec002791b276c407e751a39d
Denis <denisdani@users.noreply.github.com>
2026-03-01 19:00:34 +0100

    Add cn helper to merge clsx + tailwind-merge
    Introduce app/lib/utils.ts exporting cn(...inputs) which combines clsx for conditional classes and twMerge to resolve Tailwind class conflicts. Also add clsx and tailwind-merge to package.json dependencies to support the new utility.

---
+++ app/lib/utils.ts
@@ -0,0 +1,10 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * Combina classi condizionali (clsx) e risolve i conflitti
+ * tra classi Tailwind (tailwind-merge).
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
