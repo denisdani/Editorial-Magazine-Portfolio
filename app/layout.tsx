1337f02d9d4e73a4b4457d0ec826fd7cd3251a0b
Denis <denisdani@users.noreply.github.com>
2026-03-03 19:23:57 +0100

    ✨ feat: add scroll-smooth class to html element for improved scrolling experience
    The addition of the scroll-smooth class to the html element enhances the user experience by providing a smoother and more consistent scrolling effect across the application. This improves the overall usability and satisfaction of users when navigating through the app.

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body
         className={`${interFont.variable} ${playfairDisplayFont.variable} bg-background antialiased`}
       >
