f9792148c67f845cab7ffc49a399cdebd6570577
Denis <denisdani@users.noreply.github.com>
2026-03-04 08:59:18 +0100

    🚀 deploy: update import paths to use new directory structure
    The import paths have been updated to reflect the new directory structure. This change allows for better organization and scalability of the codebase.

--- app/article/[slug]/page.tsx
+++ app/article/[slug]/page.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image"
-import { getArticleBySlug } from "@/lib/articles"
 
 import { GoDotFill } from "react-icons/go"
 import { FaFacebook, FaLink, FaTwitter } from "react-icons/fa"
 
-import Author from "../../components/author"
-import Tag from "../../components/tag"
-import ShareButton from "../../components/shareButton"
-import { TableOfContents } from "../../components/tableOfContents"
+import Author from "@/app/components/author"
+import Tag from "@/app/components/tag"
+import ShareButton from "@/app/components/shareButton"
+import { TableOfContents } from "@/app/components/tableOfContents"
 
 export default function ArticlePage() {
   return (
